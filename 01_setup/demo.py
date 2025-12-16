bd52dd03eb03d45321e1a1be8621f3df79279fa2
sachinimaginet <sachin@imaginetventures.com>
2025-12-16 12:40:19 +0530

    Type Casting

--- 01_setup/demo.py
+++ 01_setup/demo.py
@@ -8,3 +8,40 @@ print("Difference is:", b - a)
 
 if a < b:
     print(f"{a} is less than {b}")
+
+print(type(a))
+
+# Data Types In Python 
+
+# int 
+
+numbers_name = 35
+print(type(numbers_name))
+
+# float
+float_number = 35.5
+print(type(float_number))
+
+# string
+string_name = "Sachin"
+print(type(string_name))
+
+# boolean 
+boolean_value = True
+print(type(boolean_value))
+
+# List
+list_name = [1,2,3,4,5]
+print(type(list_name))
+
+# Tuple
+tuple_name = (1,2,3,4,5)
+print(type(tuple_name))
+
+# Sets 
+sets_name = {1,2,3,4,5}
+print(type(sets_name))
+
+# Dictionary
+dict_name = {"name":"Sachin", "age":30}
+print(type(dict_name))
---
+++ 04_typecasting/03_demo.py
@@ -0,0 +1,23 @@
+a = 10
+print(a)
+print(type(a))
+
+
+b = str(a)
+print(b)
+print(type(b))
+
+c = "Sachin"
+print(c)
+print(type(c))
+
+# d = int(c)
+# print(d)
+# print(type(d))
+# The above code will raise a ValueError because "Sachin" cannot be converted to an
+
+e = "24"
+f = int(e)
+print(f)
+print(type(f))
+
