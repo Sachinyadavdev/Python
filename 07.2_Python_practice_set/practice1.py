135d47af1220a3a3326a085d3ba2260c7a9fa5eb
Sachin Yadav <88433447+Sachinyadavdev@users.noreply.github.com>
2025-12-26 01:05:58 +0530

    Practice Set 1 Done

--- 07.2_Python_practice_set/practice1.py
+++ 07.2_Python_practice_set/practice1.py
@@ -5,3 +5,31 @@ print("Hello, World! Welcome to Python")
 # Problem 2
 
 print("Twinkle, twinkle, little star,\nHow I wonder what you are!")
+
+# Problem 3
+
+name = "Sachin"
+
+age = 25
+
+height = 5.7
+
+is_student = True 
+
+print ("My Name is " + str(name) + ", I am " + str(age) + " years old, my height is " + str(height) + " and I am a student: " + str(is_student))   
+
+print(name)
+print(age)
+
+# Problem 4 
+
+num = "45"
+
+num = int(num)
+
+print (num + 5)
+
+# Problem 5
+
+z = input("Eznter the name of the Food you like: ")
+print("You like " + z)
