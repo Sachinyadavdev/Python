7ae812dc0e799649e0dfb0f7b4dfe7bd0aac880a
Sachin Yadav <88433447+Sachinyadavdev@users.noreply.github.com>
2025-12-27 23:20:12 +0530

    Practice

---
+++ 11_functions/09_pythonModules.py
@@ -0,0 +1,23 @@
+# Small Program to see the external modules in Python
+
+# import requests
+
+# r = requests.get("https://www.google.com")
+
+# print(r.status_code)
+# print(r)
+
+
+# if r.status_code == 200:
+#     print("Successfully accessed Google!")
+
+# else:
+#     print("failed to access Google. Status code: ", r.status_code)
+
+def sum(a, b):
+    c = a + b
+    c = 3
+    return c
+c= 4
+print("The sum is ", sum(4, 5))
+print(c)
---
+++ 11_functions/10_practiceset.py
@@ -0,0 +1,68 @@
+# Problem 1
+
+def greet(name):
+    print("hello", name)
+
+
+greet("Alice")
+
+
+# Problem 2
+
+def square(num):
+    return num * num
+
+print("The square of 5 is", square(5))
+
+# Problem 3
+
+def full_name(first_name, last_name):
+    return first_name + " " + last_name
+
+print("Full name is:", full_name("Sachin", "Yadav"))
+
+
+# Problem 4
+
+def cal_area(length , breadth = 10):
+    return length * breadth
+
+
+print("Area is: ", cal_area(5))
+
+
+# Problem 5
+
+sum = lambda x, y: x + y
+
+
+print("The sum is:", sum(4, 6))
+
+
+# Problem 6
+
+num1 = [1, 2, 3, 4, 5]
+
+squared_num = list(map(lambda x: x * x, num1))
+
+print("Squared numbers are:", squared_num)
+
+# problem 7
+
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+    
+print("Factorial of 5 is:", factorial(5))
+
+# Problem 8
+
+def sum_number(n):
+    if n==0:
+        return 0
+    else:
+        return n%10 + sum_number(n//10)
+
+print("Sum of digits is:", sum_number(98))
