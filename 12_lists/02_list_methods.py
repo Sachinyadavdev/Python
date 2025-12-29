8f3db71eabc4defce99c411cc815d6e588043789
sachinimaginet <sachin@imaginetventures.com>
2025-12-29 12:27:10 +0530

    Lists Completed

--- 12_lists/02_list_methods.py
+++ 12_lists/02_list_methods.py
@@ -1,8 +1,22 @@
-marks = [5, 2, 21, 5, 7]
-extra_marks = [53, 23, 32]
+# marks = [5, 2, 21, 5, 7]
+# extra_marks = [53, 23, 32]
 
-print(marks)
+# print(marks)
 # marks.append(63) # This will change the original list
 # marks.pop()
-marks.extend(extra_marks)
-print(marks)
+# marks.extend(extra_marks)
+# print(marks)
+
+names_class_1 = ["Alice", "Bob", "Rohan", "Shivani"]
+names_class_2 = ["David", "Eva", "Frank", "Grace"]
+
+print(names_class_1)
+print(names_class_2)
+names_class_1.extend(names_class_2)
+
+print(names_class_1)
+
+names_class_1.append("Sachin")
+names_class_1.pop(0)
+
+print(names_class_1)
--- 12_lists/03_list_comprehension.py
+++ 12_lists/03_list_comprehension.py
@@ -9,3 +9,17 @@
 table = [5*i for i in range(1, 11)]
 
 print(table)
+
+table2 = []
+
+for i in range(1,11):
+    print(f"5 x {i} = {5*i}")
+    table2.append(f"5 x {i} = {5*i}")
+
+
+print(table2) 
+
+
+table2 = [2*i for i in range(1,11)]
+
+print(table2)
