259fc49b4ada9629d775fe9518bcd9ead70bea52
sachinimaginet <sachin@imaginetventures.com>
2025-12-29 12:00:35 +0530

    Working on the lists

--- 12_lists/01_lists.py
+++ 12_lists/01_lists.py
@@ -1,6 +1,23 @@
-marks = [54, 23, 64, 93, 32]
-mixed = [43, "Hello", False, 4.2]
+# marks = [54, 23, 64, 93, 32]
+# mixed = [43, "Hello", False, 4.2]
 
-print(marks[2:4])
-print(marks[2])
-print(mixed[4]) # Error Index out of bound
+# print(marks[2:4])
+# print(marks[2])
+# print(mixed[4]) # Error Index out of bound
+
+
+names = ["Alice", "Bob", "Rohan", 23, "Shivani"]
+
+print(names[1:3])
+print(names[3])
+
+print(names)
+
+names[0] = "sachin"
+
+print(names)
+
+while names.count("Rohan") > 0:
+      names.remove("Rohan")
+
+print(names)
