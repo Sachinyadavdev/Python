a88adb5e2da8d6eab447578dbe64824b72624222
Sachin Yadav <88433447+Sachinyadavdev@users.noreply.github.com>
2026-01-01 21:54:51 +0530

    Done

--- 15_dictionaries/Bonus_questions.py
+++ 15_dictionaries/Bonus_questions.py
@@ -17,3 +17,12 @@ product_price2 = {"cycle":800, "scooter":1500, "skateboard":300}
 combine_prices = {**product_price, **product_price2}
 
 print("Combined product prices:", combine_prices)
+
+# Problem 4
+
+student_scores = {"Alice":85, "Bob":92, "Charlie":78, "David":90}
+
+print("Original student scores:", student_scores)
+
+top_scorer = max(student_scores, key = student_scores.get)
+print("Top scorer:", top_scorer)  
