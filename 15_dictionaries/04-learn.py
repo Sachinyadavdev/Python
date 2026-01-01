71d8034255d7e126879122de6425768612b85de2
Sachin Yadav <88433447+Sachinyadavdev@users.noreply.github.com>
2026-01-01 10:04:52 +0530

    Commit the list

--- 15_dictionaries/04-learn.py
+++ 15_dictionaries/04-learn.py
@@ -9,3 +9,13 @@ print(fruits)
 fruits[1] = 'orange'
 
 print(fruits)  # ['apple', 'orange', 'cherry', 'date']
+
+print(len(fruits))
+
+# Problem 2
+
+numbers = [1,2,3,4,5,6,7,8,9,10]
+
+print(numbers)
+
+print(numbers[0:3])
