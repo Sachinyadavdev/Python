f3a387aaefed4c500f93f6fb94d4296400a30ffb
sachinimaginet <sachin@imaginetventures.com>
2026-01-02 12:02:39 +0530

    Updated

---
+++ 16_oops/07_constructor_learn.py
@@ -0,0 +1,17 @@
+
+class Employee:
+    def __init__(self,name,age,salary):
+        self.name = name
+        self.age = age
+        self.salary = salary
+
+    def employee_details(self):
+        return f"Employee Name: {self.name}, Age: {self.age}, Salary: {self.salary}"
+
+name = input("Enter Name: ")
+age = int(input("Enter Age: "))
+salary = int(input("Enter Salary: "))
+
+e1 = Employee(name, age, salary)
+
+print(e1.employee_details())
