be83af54cc9d2e5c771286f7e01a4f7a9cdedc13
Sachin Yadav <sachinyadavshiv8@gmail.com>
2026-01-10 20:16:24 +0530

    Practice on the default Parameteriezed Constructors

--- 16_oops/08_inheritance_practice.py
+++ 16_oops/08_inheritance_practice.py
@@ -27,3 +27,19 @@ cat = Cat("Whiskers", "Tabby")
 
 print(dog.speak())  # Output: Buddy barks
 print(cat.speak())  # Output: Whiskers meows
+
+# Default Values Parameter for the Constructor
+
+class Bird(Animal):
+    def __init__ (self, name, species="Parrot"):
+        super().__init__(name)
+        self.species = species
+
+    def speak(self):
+        return f"{self.name} chirps, and is a {self.species}"
+    
+bird1 = Bird("Polly")
+bird2 = Bird("Tweety", "Canary")
+
+print(bird1.speak())  # Output: Polly chirps, and is a Parrot
+print(bird2.speak())  # Output: Tweety chirps, and is a
