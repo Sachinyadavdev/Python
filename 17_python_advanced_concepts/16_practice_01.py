33c799f42c1b7acc4ff8f526a4ae55b52d33ea69
Sachin Yadav <sachinyadavshiv8@gmail.com>
2026-01-21 08:23:50 +0530

    Decorators With The Arguments

--- 17_python_advanced_concepts/16_practice_01.py
+++ 17_python_advanced_concepts/16_practice_01.py
@@ -10,3 +10,17 @@ def say_hello():
     print("Hello!")
 
 say_hello()
+
+def repeat(n):
+    def decorator(func):
+        def warpper(arg):
+            for _ in range(n):
+                func(arg)
+        return warpper
+
+    return decorator    
+
+@repeat(3)
+def greet(name):
+    print(f"Hello, {name}!")
+greet("Alice")
