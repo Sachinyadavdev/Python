f33236208050e1653f6af7a60a3fe3a251f01ba1
Sachin Yadav <sachinyadavshiv8@gmail.com>
2026-04-15 18:43:15 +0530

    Changes and learn the Python

--- 02_python_syntax/main.py
+++ 02_python_syntax/main.py
@@ -7,3 +7,18 @@ print(3)
 #     statement1
 #     statement3
 # statement2
+
+# Tetsing The Python Syntax
+
+a = 30
+b = 20
+
+if a > b:
+    print("a is greater than b",a)
+elif a == b:
+    print("a is equal to b")
+else:
+    print("a is less than b",b)
+
+
+# In Python Syntaxing and intendation is important 
