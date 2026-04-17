effb5bb2eb3af1af3710962ef6d80b69e35dc1f2
Sachin Yadav <sachinyadavshiv8@gmail.com>
2026-04-17 11:29:42 +0530

    Updated the Getter and Setter

--- 17_python_advanced_concepts/03.1_getter_and_setter.py
+++ 17_python_advanced_concepts/03.1_getter_and_setter.py
@@ -3,11 +3,26 @@ class Employee:
         self.name = name 
         self.salary = salary
 
+    @property
     def first_name(self):
         l = self.name.split(" ") 
         return l[0]
-        
+    
+    @first_name.setter
+    def first_name(self, first):
+        l = self.name.split(" ")
+        new_name = f"{first} {l[1]}" 
+        self.name = new_name
+
+
 
 e = Employee("Jack Doe", 34555)
-firstName = e.first_name()
-print(firstName)
+# firstName = e.first_name()
+# print(firstName)
+
+# e.set_fist_name("John")
+# print(e.name)
+
+print(e.first_name)
+e.first_name = "John"
+print(e.name)
