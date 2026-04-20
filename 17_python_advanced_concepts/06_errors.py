447cec7fd3002c13ab1e2ced6e6ba74d96d873f5
Sachin Yadav <sachinyadavshiv8@gmail.com>
2026-04-20 10:55:29 +0530

    Updated the python code for the practice

--- 17_python_advanced_concepts/06_errors.py
+++ 17_python_advanced_concepts/06_errors.py
@@ -13,10 +13,36 @@
 #     except Exception as e:
 #         print("Unknown error occurred!", e)
 
+# a = int(input("Enter number 1: "))
+# b = int(input("Enter number 2: "))
+
+# if b == 0:
+#     raise ValueError("Please dont divide by 0")
+
+# print(f"The division is {a / b}")
+
+# Practice 
+
+# while True:
+#     try:
+#         a = int(input("Enter numbe 1: "))
+#         b = int(input("Enter number 2: "))
+
+#         print(f"The sum is {a + b}")
+
+#     except ValueError:
+#         print("Please dont perform bad typecasts")
+
+#     except Exception as e:
+#         print("Something went wrong:", e)
+
 a = int(input("Enter number 1: "))
 b = int(input("Enter number 2: "))
 
-if b == 0:
-    raise ValueError("Please dont divide by 0")
+if a < 0 or b < 0:
+    raise ValueError("Please dont enter negative numbers")
+elif a == 0 or b == 0:
+    raise ValueError("Please dont enter 0")
+else:
+    print(f"The sum is {a + b}")
 
-print(f"The division is {a / b}")
