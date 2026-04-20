4f2821baddcb5f515e4555a53d79a346f361702f
Sachin Yadav <sachinyadavshiv8@gmail.com>
2026-04-20 11:51:51 +0530

    New Changes in the map and filter

--- 17_python_advanced_concepts/09_map.py
+++ 17_python_advanced_concepts/09_map.py
@@ -6,3 +6,14 @@ numbers = [1, 2, 3, 45, 4, 21]
 
 new = list(map(lambda x: x*x, numbers))
 print(new)
+
+# Practice 
+
+cube_numbers = [1, 2, 3, 4, 5]
+
+def cube(x):
+    return x * x * x
+
+new_cubes = list(map(cube, cube_numbers))
+
+print(new_cubes)
