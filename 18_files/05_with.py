4ca5f47236223bdd6946066ac38cf3cf54d3579a
Sachin Yadav <sachinyadavshiv8@gmail.com>
2026-04-21 11:29:52 +0530

    Learn the OS and Shutil Modules in the Python

--- 18_files/05_with.py
+++ 18_files/05_with.py
@@ -3,7 +3,12 @@
 # print(content)
 # f.close()
 
-with open("harry.txt", "r") as f: # context manager
+# with open("harry.txt", "r") as f: # context manager
+#     content = f.read()
+#     print(content)
+    # No need to write f.close() because file is already closed by default when using with synax
+
+with open("sachin.txt","r") as f:
     content = f.read()
     print(content)
-    # No need to write f.close() because file is already closed by default when using with synax
+
--- 18_files/06_os.py
+++ 18_files/06_os.py
@@ -1,9 +1,10 @@
 import os 
 
-
 a = os.listdir("dir")
 print(a)
 print(os.getcwd())
-print(os.path.exists("harry"))
+print(os.path.exists("harry.txt"))
 # os.remove("sample.txt")
-os.rmdir("dir")
+# os.rmdir("dir")
+b = os.open("harry.txt", os.O_RDWR)
+print(b)
