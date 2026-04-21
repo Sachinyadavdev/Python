a65aa009d3ebeabcad54b07ec4ff588731641155
Sachin Yadav <sachinyadavshiv8@gmail.com>
2026-04-21 10:19:42 +0530

    Updated the python code in the File handling

--- 18_files/01_read.py
+++ 18_files/01_read.py
@@ -1,7 +1,28 @@
-f = open("harry.txt", "r")
+# f = open("harry.txt", "r")
 
-content = f.read()
+# content = f.read()
 
-print(content)
+# print(content)
 
+# f.close()
+
+# Practice 
+
+# f = open("sachin.txt", "rt")
+
+# content = f.read()
+
+# print(content)
+
+# for line in f:
+#     print(line)
+
+# f.close()
+
+# f= open("sachin.txt", "w")
+f= open("sachin.txt", "a")
+
+string = '''Sachin is a great cricketer. He is the best batsman in the world. He has scored 100 centuries in international cricket. He is also known as the God of Cricket.'''
+
+f.write(string)
 f.close()
