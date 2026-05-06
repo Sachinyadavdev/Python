70f2f4a4a9dc8d457b0ce4b4d3b5522606333d01
Sachin Yadav <sachinyadavshiv8@gmail.com>
2026-05-06 15:10:39 +0530

    updated the Swap Function code

--- 0_A_Python_Practice_Codes/1_Practice.py
+++ 0_A_Python_Practice_Codes/1_Practice.py
@@ -4,15 +4,19 @@ a = 10
 b = 5
 
 
-c = a
-a = b
-b = c
+# c = a
+# a = b
+# b = c
 
+def swap(a,b):
+    return b,a
 
 print("The value of a :",a)
 print("The value of b :",b)
 
 # a,b = b,a  Pythonic login for Swapping two numbers
+
+a,b = swap(a,b)
 
 print(a)
 print(b)
