93cc4247b843caa7546849162eea981932a9f89a
Sachin Yadav <sachinyadavshiv8@gmail.com>
2026-05-06 12:39:17 +0530

    Practice the Two Number Swapping

---
+++ 0_A_Python_Practice_Codes/1_Practice.py
@@ -0,0 +1,18 @@
+# Challenge 1: Variable Swap
+
+a = 10
+b = 5
+
+
+c = a
+a = b
+b = c
+
+
+print("The value of a :",a)
+print("The value of b :",b)
+
+# a,b = b,a  Pythonic login for Swapping two numbers
+
+print(a)
+print(b)
