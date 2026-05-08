5b64ad60c43a49feec69ddc993db77a021472594
Sachin Yadav <sachinyadavshiv8@gmail.com>
2026-05-09 04:21:42 +0530

    Completed the Day 1 Challenge

---
+++ 0_A_Python_Practice_Codes/3_Practice.py
@@ -0,0 +1,11 @@
+# Create three different variables: one holding a whole number (Integer), one holding a decimal (Float), and one holding text (String). Use the type() function to print out the class of each variable.
+
+int_num = 10
+
+float_num = 3.67
+
+string_name = "Sachin Yadav"
+
+print(type(int_num))
+print(type(float_num))
+print(type(string_name))
