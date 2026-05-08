ebdc272641dd036172b38a841b4affe567dc0224
Sachin Yadav <sachinyadavshiv8@gmail.com>
2026-05-08 13:35:51 +0530

    Solve the Challenge 2 Question

--- 0_A_Python_Practice_Codes/2_Practice.py
+++ 0_A_Python_Practice_Codes/2_Practice.py
@@ -1,2 +1,11 @@
 # Challenge 2: The Tip Calculator
 
+# Create a program where you define a bill_amount and a tip_percentage. Calculate the total amount to be paid.
+
+bill_amount = 1000
+
+bill_tip = 0.15
+
+cal = 1000 + bill_amount * bill_tip
+
+print("The bill amount after calculating the tip: ",cal)
