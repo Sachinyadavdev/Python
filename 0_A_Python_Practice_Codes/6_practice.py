dbde96a49529bab6b4814f95b51a5a098baa81d7
Sachin Yadav <sachinyadavshiv8@gmail.com>
2026-05-17 10:58:21 +0530

    Day 3 all challenges has been completed

--- 0_A_Python_Practice_Codes/6_practice.py
+++ 0_A_Python_Practice_Codes/6_practice.py
@@ -19,3 +19,34 @@ fruits[1] = "blueberry"
 print(fruits)
 
 print(len(fruits))
+
+
+# Challenge 2: The Multiplier (For Loops)Task: Use a for loop and the range() function to print the multiplication table of 5 (from $5 \times 1$ to $5 \times 10$).Expected Output:51015... and so on.
+
+for i in range(1,11):
+    print(5*i)
+
+
+# Challenge 3: The Sum Collector (While Loops)
+# Task: Create a script that keeps asking the user to "Enter a number to add to the total" or type "exit" to stop.
+
+# Keep a running total variable.
+
+# When the user types "exit", stop the loop and print the final sum.
+
+# Hint: You'll need to convert the input to an int() only if it's not "exit".
+
+total_sum =0
+
+while True:
+
+    user_input = input("Enter the number you want to add or type exit to exit the loop: ")
+
+    if user_input == "exit":
+        break
+
+    else:
+        user_input = int(user_input)
+        total_sum += user_input
+
+print(f"The total sum is: {total_sum}")
