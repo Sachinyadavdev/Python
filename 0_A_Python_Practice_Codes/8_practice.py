30385713c01c769f0d50158d1c2a9e877f977792
Sachin Yadav <88433447+Sachinyadavdev@users.noreply.github.com>
2026-06-05 17:54:50 +0530

    Chnages has ben made

--- 0_A_Python_Practice_Codes/8_practice.py
+++ 0_A_Python_Practice_Codes/8_practice.py
@@ -22,3 +22,38 @@ user_profile["Experience"] = 4
 user_profile["Village"] = "Amarupur"
 
 print("Updated Profile:", user_profile)
+
+# Challenge 2: Word Frequency Counter (Looping over Data)
+# Task: Write a script that takes a list of strings and counts how many times each item appears, storing the results in a dictionary.
+
+# Input List: ["python", "javascript", "python", "java", "python", "javascript"]
+
+# Expected Output: {'python': 3, 'javascript': 2, 'java': 1}
+
+# Hint: Start with an empty dictionary {}. Loop through the list; if the word is already a key in your dictionary, increase its value by 1. If it's not, add it with a value of 1.
+
+words = ["python","javascript", "java", "sachin", "sachin", "sachin", "sachin"]
+
+frequency ={}
+
+for word in words:
+    if word in frequency:
+        frequency[word] +=1
+
+    else:
+        frequency[word] = 1
+
+print(frequency)
+
+# Challenge 3: Nested Data Extraction
+# Task: In real-world software development, data often arrives as nested structures (like JSON from an API). Given the following dictionary, write a single print statement that extracts and displays the second tool in the developer's tech stack.
+
+developer_data = {
+    "employee_id": 1024,
+    "skills": {
+        "primary": "Backend",
+        "tools": ["Git", "Docker", "Firebase"]
+    }
+}
+
+print(developer_data["skills"]["tools"][1])
