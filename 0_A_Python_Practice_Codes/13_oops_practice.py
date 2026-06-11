31f11647ce90ad6bd3485069975991c5879f2285
Sachin Yadav <88433447+Sachinyadavdev@users.noreply.github.com>
2026-06-11 18:49:59 +0530

    Updated the OOPs topics

--- 0_A_Python_Practice_Codes/13_oops_practice.py
+++ 0_A_Python_Practice_Codes/13_oops_practice.py
@@ -7,3 +7,71 @@
 
 # Print the object's brand and model.
 
+class Car:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+    
+    def drive(self):
+        print(f"{self.brand} {self.model} is driving on the road")
+
+Tesla1 = Car("Tesla","Model 3")
+
+print(Tesla1.brand)
+print(Tesla1.model)
+
+Tesla1.drive()
+
+
+
+
+# Challenge 2: Giving the Car a Voice
+# Task: Expand your Car class by adding a custom method inside it.
+
+# Add a method called drive(self).
+
+# This method should print out a message using the car's properties, like: "The Tesla Model 3 is now driving!"
+
+# Call this method using the object you created in Challenge 1.
+
+
+# Challenge 3: The Bank Account (Data Encapsulation)
+# Task: Create a class called BankAccount.
+
+# The constructor __init__ should take an argument for the owner_name and set an initial property balance = 0.
+
+# Add a method called deposit(self, amount) that adds money to the balance and prints the new balance.
+
+# Add a method called withdraw(self, amount) that subtracts money from the balance.
+
+# Bonus Condition: Inside withdraw, add an if statement to check if there is enough money. If amount > balance, print "Insufficient funds!" instead of subtracting.
+
+class BankAccount:
+    def __init__(self, user_name):
+        self.name = user_name
+        self.balance = 0
+
+    def deposit(self, amount):
+        
+        if amount < 0:
+            print("Invalid Amount Entered")
+
+        else:
+            self.balance = self.balance + amount
+
+        print(f"Updated Balance is: {self.balance} " )
+
+    def withdraw(self, amount):
+        
+        if amount > self.balance:
+            print("Insufficient Balance")
+
+        else:
+            self.balance = self.balance - amount
+
+        print(f"Updated Balance is: {self.balance}")
+
+account1 = BankAccount("Sachin")
+
+account1.deposit(10000)
+account1.withdraw(100000)
