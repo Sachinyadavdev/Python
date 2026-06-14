59426213d193638a25545f73f1a680656ac94d6a
Sachin Yadav <88433447+Sachinyadavdev@users.noreply.github.com>
2026-06-14 11:03:58 +0530

    Updated the Code

--- 0_A_Python_Practice_Codes/17_oops_practice.py
+++ 0_A_Python_Practice_Codes/17_oops_practice.py
@@ -16,3 +16,33 @@ for i in range(0,transactions.__len__()):
     total = transactions[i]['amount'] + total
 
 print(f"The total amount that has been calculated is: {total}")
+
+
+# Challenge 2: The Successful Filter
+# Task: Create a new list called successful_items. Loop through the transactions and add the "item" name to your new list only if its "status" is exactly "completed".
+
+# Expected Output: ['Laptop', 'Monitor', 'Keyboard']
+
+# Bonus: Can you write this as a single-line list comprehension?
+
+successful_items = [items['item'] for items in transactions if items['status'] == "completed"]
+
+print(successful_items)
+
+
+# Challenge 3: The Expensive Finder
+# Task: Write a script that finds and prints the dictionary of the single most expensive item in the list.
+
+# Expected Output: {"id": 1, "item": "Laptop", "amount": 1200, "status": "completed"}
+
+# Hint: Keep a variable like most_expensive = transactions[0] and loop through the rest, comparing the "amount" key.
+
+# This mimics exactly what data analysts and backend developers do every day. Copy the transactions list into your environment and give these three a shot!
+
+most_expensive = transactions[0]
+
+for t in transactions:
+    if t["amount"] > most_expensive["amount"]: 
+        most_expensive = t
+
+print(f"The most expensive item is {most_expensive['item']} and its price is {most_expensive['amount']}")
