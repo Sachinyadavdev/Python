93190e08268222d61d8fa118b4c837ff7025fa49
Sachin Yadav <88433447+Sachinyadavdev@users.noreply.github.com>
2026-06-24 17:05:35 +0530

    Updated the Code

--- 0_A_Python_Practice_Codes/19_flask_1.py
+++ 0_A_Python_Practice_Codes/19_flask_1.py
@@ -6,5 +6,15 @@ app = Flask(__name__)
 def home():
     return "Welcome to the Home Page"
 
+
+@app.route("/contact")
+def contact():
+    return "This is the Contact Page"
+
+@app.route("/user/<name>")
+def user(name):
+    return f"Hello, {name}!, Welcome to the Users page"
+
 if __name__ == "__main__":
     app.run(debug=True)
+
