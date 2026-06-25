7fbf8ebaa99d60631cab3146810c33d560fc2d28
Sachin Yadav <88433447+Sachinyadavdev@users.noreply.github.com>
2026-06-25 16:42:37 +0530

    Updated the Code

--- 0_A_Python_Practice_Codes/19_flask_1.py
+++ 0_A_Python_Practice_Codes/19_flask_1.py
@@ -15,6 +15,12 @@ def contact():
 def user(name):
     return f"Hello, {name}!, Welcome to the Users page"
 
+
+@app.route("/user/<name>/<int:age>")
+def user_age(name, age):
+   return f"Hello {name}!, your age is {age} years old"
+
+
 if __name__ == "__main__":
     app.run(debug=True)
 
