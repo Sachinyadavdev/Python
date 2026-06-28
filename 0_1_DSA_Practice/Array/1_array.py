5982b5b628bb6ac92ccae7117bc1828e7c472406
Sachin Yadav <88433447+Sachinyadavdev@users.noreply.github.com>
2026-06-28 11:00:48 +0530

    Updated the Code

---
+++ 0_1_DSA_Practice/Array/1_array.py
@@ -0,0 +1,9 @@
+import array as arr
+
+# Creating an array with integer type
+my_array = arr.array('i',[1, 2, 3, 4, 5])
+# print("Array elements:")
+# for element in my_array:
+#     print(element)
+
+print(my_array)
