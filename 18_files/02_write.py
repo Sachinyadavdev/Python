955e94ea64662402a9498c75f3198d7b1dd50fad
Sachin Yadav <88433447+Sachinyadavdev@users.noreply.github.com>
2026-09-10 13:58:43 +0530

    Updated the Code

--- 18_files/02_write.py
+++ 18_files/02_write.py
@@ -1,7 +1,7 @@
 # Write to a file called John Doe.txt 
 # It should contain data about John Doe
 
-f = open("John Doe.txt", "w")
+f = open("C:\\Users\\YC COMPUTER\\Documents\\GitHub\\Python\\18_files\\John Doe3.txt", "w")
 
 string = '''
 John Doe is a nice guy. He lives in Nyc and he works with Python
